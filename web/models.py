f9667b00887dd3994e2b157b89e90ed4724648d4
songryu <108215485+songryu@users.noreply.github.com>
2022-11-02 19:45:17 +0900

    form으로 엑셀 업로드

--- web/models.py
+++ web/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.db.models import Model
 # Create your models here.
+
+class Document(models.Model):
+    uploadedFile = models.FileField('첨부파일',upload_to = "files/")
--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
+from . import views
 from .views import *
 app_name='web'
 urlpatterns = [
-    path('test',test,name='test'),
+    path('webtest',upload_file,name='webtest'),
+    #path('postcreate/',views.test,name='test')
 ]
